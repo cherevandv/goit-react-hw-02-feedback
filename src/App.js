bcc7bea3b68c511b93463afef5d60f1e2ced82e0
Daria Kobekhno <cherevandv@gmail.com>
2020-12-08 11:57:02 +0200

    added components

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react';
+
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Notification from './components/Notification/Notification';
+import Statistics from './components/Statistics/Statistics';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = ({ target }) => {
+    const type = target.name;
+    this.setState(prev => ({
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () => {
+    const positive = this.state.good;
+    return positive && Math.round((positive * 100) / this.countTotalFeedback());
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    // const positiveFeedback = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div className={s.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {totalFeedback === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+const { Fragment } = require('react');
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <Fragment>
+    {options.map(option => (
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={onLeaveFeedback}
+      >
+        {option}
+      </button>
+    ))}
+  </Fragment>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
