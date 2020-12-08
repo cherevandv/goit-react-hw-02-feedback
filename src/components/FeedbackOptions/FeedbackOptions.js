2a713db3dc3c6429892c0a05a8152d6fd328c576
Daria Kobekhno <cherevandv@gmail.com>
2020-12-08 14:20:46 +0200

    added styles

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
 const { Fragment } = require('react');
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <Fragment>
-    {options.map(option => (
-      <button
-        key={option}
-        type="button"
-        name={option}
-        onClick={onLeaveFeedback}
-      >
-        {option}
-      </button>
-    ))}
+    {options.map(option => {
+      const arrClasses = [s.button];
+      if (option === 'good') {
+        arrClasses.push(s.button_good);
+      }
+      if (option === 'bad') {
+        arrClasses.push(s.button_bad);
+      }
+      return (
+        <button
+          className={arrClasses.join(' ')}
+          key={option}
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      );
+    })}
   </Fragment>
 );
 
